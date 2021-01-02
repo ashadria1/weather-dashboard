943395ab05b23aa3171e086abf31d11bac9a30c4
ashadria <ashadria@gmail.com>
2021-01-01 19:31:42 -0500

    pushing templates

---
+++ javascript-template.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    console.log("Hello world! The document is ready!");
+    // /*
+    // Comments go here.
+    // Comments go here.
+    // Comments go here.
+    // Comments go here.
+    // Comments go here.
+    // Comments go here.
+    // Comments go here.
+    // */
+})
